b3470b0065662cc8c95ba6a8cc21b049adeee254
pelhu <khamzat@live.com>
2017-03-21 10:37:56 +0300

    .

--- src/WebHdfs.Core/publishPackage.ps1
+++ src/WebHdfs.Core/publishPackage.ps1
@@ -1,12 +1,21 @@
-$versionPattern = "(""version"": "".+?)-(\d+)("")"
-$prj = Get-Content $PSScriptRoot\project.json
+#$versionPattern = "(""version"": "".+?)-(\d+)("")"
+#$prj = Get-Content $PSScriptRoot\project.json
 
-$version = [int]($prj| select-string $versionPattern).matches[0].Groups[2].Value
+#$version = [int]($prj| select-string $versionPattern).matches[0].Groups[2].Value
 
-$prj -replace $versionPattern,('$1-'+($version+1)+'$3') | Set-Content $PSScriptRoot\project.json
+#$prj -replace $versionPattern,('$1-'+($version+1)+'$3') | Set-Content $PSScriptRoot\project.json
 
-dotnet build $PSScriptRoot\. -c Release
-dotnet pack $PSScriptRoot\. -c Release
+$prjName = Split-Path $PSScriptRoot -Leaf
+$versionPattern = "(\<VersionPrefix\>.+?)-(\d+)(\<)"
+$prjPath = "$PSScriptRoot\$prjName.csproj"
+$prjContent = Get-Content $prjPath
+
+$version = [int]($prjContent| select-string $versionPattern).matches[0].Groups[2].Value
+
+$prjContent -replace $versionPattern,('$1-'+($version+1)+'$3') | Set-Content $prjPath
+
+#dotnet build $PSScriptRoot\. -c Release
+dotnet pack $PSScriptRoot\. -c Release --include-symbols
 
 $filename = gci $PSScriptRoot\bin\Release\*.symbols.nupkg | sort Name | select -last 1
 
